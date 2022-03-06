dd489646f5ed84387097f57ac1cf49d68104d925
Naveen-2 <naveen2k15cse@gmail.com>
2022-03-07 01:14:45 +0530

    Binary search tree can be created

---
+++ BST/src/com/Main.java
@@ -0,0 +1,23 @@
+package com;
+
+
+public class Main {
+
+    /**
+     * Main method for manipulation linkedList
+     * @param args - Default Java param (Not used)
+     */
+    public static void main(String[] args) {
+        /*
+        Creating a new binary tree
+         */
+        BST<Integer> binarySearchTree = new BST<>();
+
+        /*
+        Adding data to binary tree as in UC1.
+         */
+        binarySearchTree.add(56);
+        binarySearchTree.add(30);
+        binarySearchTree.add(70);
+    }
+}
